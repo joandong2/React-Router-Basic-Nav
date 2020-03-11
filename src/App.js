92ba92d13b422219a6cf0033f9e42d5e16a81874
John Oblenda <joandong2@gmail.com>
2020-03-10 22:01:09 -0400

    added routers and navigation links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import { Home, About, Contact, Navigation } from "./components";
+
+import "./App.css";
 
 const App = () => (
-  <div>
-    <Navigation />
-  </div>
+    <div>
+        <Navigation />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+    </div>
 );
 
 export default App;
